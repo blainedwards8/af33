a8152c5d07e33e7666bca12d5eecd0e17863832e
Blaine <blainedwards8@gmail.com>
2021-01-10 20:36:50 -0700

    Added initial routing

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var express = require('express');
-var app = express();
+var Datastore = require('nedb-promise');
+const path = require('path');
 
-app.get('/:link',(req,res) => {
-    
-    res.redirect('/');
+var app = express();
+var linksdb = new Datastore({filename: "./dbs/links.db", autoload: true});
+
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+
+app.get('/', async (req,res) => {
+    let links = await linksdb.find({});
+    res.render("index.ejs", {links});
 });
 
-app.use('/',express.static('/public'));
+app.get('/:link', async (req,res) => {
+    let {link} = req.params;
+    let record = await linksdb.find({link});
+
+    if(!!record && !!record.url) res.redirect(record.url);
+    else res.sendStatus(404);
+});
 
 
 app.listen(3000, () => {
